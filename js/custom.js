f45ac78486d01067d08b05fd2bb22932fa4e5c66
geofflightbourn <glightbourn80@gmail.com>
2016-06-20 03:13:03 -0500

    fixed CSS

--- js/custom.js
+++ js/custom.js
@@ -1,56 +1,67 @@
-var templateSource = document.getElementById('results-template').innerHTML,
-    template = Handlebars.compile(templateSource),
-    resultsPlaceholder = document.getElementById('results'),
-    playingCssClass = 'playing',
-    audioObject = null;
+$( document ).ready(function() {
 
-var fetchTracks = function (albumId, callback) { //get albums
-    $.ajax({
-        url: 'https://api.spotify.com/v1/albums/' + albumId,
-        success: function (response) {
-            callback(response);
-        }
-    });
-};
+  var templateSource = document.getElementById('results-template').innerHTML,
+      template = Handlebars.compile(templateSource),
+      resultsPlaceholder = document.getElementById('results'),
+      playingCssClass = 'playing',
+      audioObject = null;
 
-var searchAlbums = function (query) { //search
-    $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-            q: query,
-            type: 'album'
-        },
-        success: function (response) {
-            resultsPlaceholder.innerHTML = template(response);
-        }
-    });
-};
+  var fetchTracks = function (albumId, callback) { //get albums
+      $.ajax({
+          url: 'https://api.spotify.com/v1/albums/' + albumId,
+          success: function (response) {
+              callback(response);
+          }
+      });
+  };
 
-results.addEventListener('click', function(e) {
-    var target = e.target;
-    if (target !== null && target.classList.contains('cover')) {
-        if (target.classList.contains(playingCssClass)) {
-            audioObject.pause();
-        } else {
-            if (audioObject) {
-                audioObject.pause();
-            }
-            fetchTracks(target.getAttribute('data-album-id'), function(data) {
-                audioObject = new Audio(data.tracks.items[0].preview_url);
-                audioObject.play();
-                target.classList.add(playingCssClass);
-                audioObject.addEventListener('ended', function() {
-                    target.classList.remove(playingCssClass);
-                });
-                audioObject.addEventListener('pause', function() {
-                    target.classList.remove(playingCssClass);
-               });
-            });
-        }
-    }
+  var searchAlbums = function (query) { //search
+      $.ajax({
+          url: 'https://api.spotify.com/v1/search',
+          data: {
+              q: query,
+              type: 'album'
+          },
+          success: function (response) {
+              resultsPlaceholder.innerHTML = template(response);
+          }
+      });
+  };
+
+  results.addEventListener('click', function(e) {
+      var target = e.target;
+      if (target !== null && target.classList.contains('cover')) {
+          if (target.classList.contains(playingCssClass)) {
+              audioObject.pause();
+          } else {
+              if (audioObject) {
+                  audioObject.pause();
+              }
+              fetchTracks(target.getAttribute('data-album-id'), function(data)  {
+                  audioObject = new Audio(data.tracks.items[0].preview_url);
+                  audioObject.play();
+                  target.classList.add(playingCssClass);
+                  audioObject.addEventListener('ended', function() {
+                      target.classList.remove(playingCssClass);
+                  });
+                  audioObject.addEventListener('pause', function() {
+                      target.classList.remove(playingCssClass);
+                 });
+              });
+          }
+      }
+  });
+
+  var $loading = $('#loadingDiv').hide();
+$(document).ajaxStart(function () {
+    $loading.show();
+  }).ajaxStop(function () {
+    $loading.hide();
+  });
+
+  document.getElementById('search-form').addEventListener('submit', function (e) {
+      e.preventDefault();
+      searchAlbums(document.getElementById('query').value);
+  }, false);
+
 });
-
-document.getElementById('search-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    searchAlbums(document.getElementById('query').value);
-}, false);

c21b2a028904de5b1b1695c65de62d9aaa081da0
geofflightbourn <glightbourn80@gmail.com>
2016-06-26 00:59:14 -0500

    <p> problem

--- js/custom.js
+++ js/custom.js
@@ -28,66 +28,88 @@ $( document ).ready(function() {
     })
   };
 
-var searchAlbums = function (query) { //search
-  $.ajax({
-    url: 'https://api.spotify.com/v1/search',
-    data: {
-      q: query,
-      type: 'album'
-    },
-    success: function (response) {
-      resultsPlaceholder.innerHTML = template(response); // Almbum covers
-    }
-
-  });
-};
-
-results.addEventListener('click', function(e) { //30 second preview function
-  var target = e.target;
-  if (target !== null && target.classList.contains('cover')) {
-
-    var albumId = target.getAttribute('data-album-id');
-    var uri = 'https://embed.spotify.com/?theme=white&uri=' + encodeURIComponent('spotify:album:' + albumId);
-    $('#player').attr('src', uri);
-
-    // var relseaseDate = target.getAttribute('data-popularity');
-    // $('#stats').append('<p>' + popularity + '</p>')
-
-    if (target.classList.contains(playingCssClass)) { //preview_url
-      audioObject.pause();
-    } else {
-      if (audioObject) {
-        audioObject.pause();
+  var searchAlbums = function (query) { //search
+    $.ajax({
+      url: 'https://api.spotify.com/v1/search',
+      data: {
+        q: query,
+        type: 'album'
+      },
+      success: function (response) {
+        resultsPlaceholder.innerHTML = template(response); // Almbum covers
       }
-      fetchTracks(albumId, function(data)  {
-        // data.albumData
-        // data.artistData
-        debugger;
-        $('#stats').append('<p>'+ data.albumData.release_date +'<p>')
-        audioObject = new Audio(data.albumData.tracks.items[0].preview_url);
-        audioObject.play();
-        target.classList.add(playingCssClass);
-        audioObject.addEventListener('ended', function() {
-          target.classList.remove(playingCssClass);
+
+    });
+  };
+
+  results.addEventListener('click', function(e) { //click event
+    var target = e.target;
+    if (target !== null && target.classList.contains('cover')) {
+
+      var albumId = target.getAttribute('data-album-id');
+      var uri = 'https://embed.spotify.com/?theme=white&uri=' + encodeURIComponent('spotify:album:' + albumId);
+      $('#player').attr('src', uri);
+
+      if (target.classList.contains(playingCssClass)) { //preview_url
+        audioObject.pause();
+      } else {
+        if (audioObject) {
+          audioObject.pause();
+        }
+        fetchTracks(albumId, function(data)  {
+          debugger;
+          $('#albumStats').empty().remove("p:even") //append html to page
+
+          var albumStats = (
+          '<h6> Title </h6>' +
+          '<p>'+ data.albumData.name +'<p>' +
+          '<h6> Date </h6>' +
+          '<p>'+ data.albumData.release_date + '<p>' +
+          '<h6> Label </h6>' +
+          '<p>'+ data.albumData.copyrights[0].text +'<p>' +
+          '<h6> Popularity </h6>' +
+          '<p>'+ data.albumData.popularity +'<p>'
+        )
+          $('#albumStats').append(albumStats)
+
+          var artistStats = (
+            '<h6> Name </h6>' +
+            '<p>'+ data.artistData.name +'<p>' +
+            '<h6> Genre </h6>' +
+            '<p>'+ data.artistData.genres[1] +'<p>' +
+            '<h6> Spotify Popularity </h6>' +
+            '<p>'+ data.artistData.popularity +'<p>' +
+            '<h6> Spotify Followers </h6>' +
+            '<p>'+ data.artistData.followers.total +'<p>' 
+          )
+            $('#artistStats').append(artistStats)
+
+
+          // end append
+          audioObject = new Audio(data.albumData.tracks.items[0].preview_url); // preview audio
+          audioObject.play();
+          target.classList.add(playingCssClass);
+          audioObject.addEventListener('ended', function() {
+            target.classList.remove(playingCssClass);
+          });
+          audioObject.addEventListener('pause', function() {
+            target.classList.remove(playingCssClass);
+          });
         });
-        audioObject.addEventListener('pause', function() {
-          target.classList.remove(playingCssClass);
-        });
-      });
+      }
     }
-  }
-}); //end preview_url
+  }); //end click event
 
-var $loading = $('#loadingDiv').hide(); //show / hide loading bar
-$(document).ajaxStart(function () {
-  $loading.show();
-}).ajaxStop(function () {
-  $loading.hide();
-});
+  var $loading = $('#loadingDiv').hide(); //show / hide loading bar
+  $(document).ajaxStart(function () {
+    $loading.show();
+  }).ajaxStop(function () {
+    $loading.hide();
+  });
 
-document.getElementById('search-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  searchAlbums(document.getElementById('query').value);
-}, false);
+  document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    searchAlbums(document.getElementById('query').value);
+  }, false);
 
 });
